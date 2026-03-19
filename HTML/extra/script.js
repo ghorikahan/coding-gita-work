8542e995edee5d83ecb9315284a4ac023a360ed1
ghorikahan <kahan.ghori.cg@gmail.com>
2026-03-19 19:09:35 +0530

    Add HTML folder

---
+++ HTML/extra/script.js
@@ -0,0 +1,8 @@
+console.log('hello world');
+window.alert('Alert');
+document.getElementById("text1").textContent =  'hello world' ;
+let age;
+age = 25;
+console.log(typeof age);
+console.log(age);
+console.log(`you are ${age} years old`);
